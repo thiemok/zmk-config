bd1632bb25c75c1e0916646c486e4c2558cd1d1e
thiemok <krausethiemo@googlemail.com>
2025-04-19 16:11:26 +0200

    fix: build issues

--- src/board_status_underglow.c
+++ src/board_status_underglow.c
@@ -4,22 +4,21 @@
  * SPDX-License-Identifier: MIT
  */
 
-#define DT_DRV_COMPAT zmk_goard_status_underglow
+#define DT_DRV_COMPAT zmk_board_status_underglow
 
 #include <zephyr/device.h>
 #include <zephyr/logging/log.h>
-#include <zmk/events/layer_state_changed.h>
 #include <zmk/event_manager.h>
+#include <zmk/events/layer_state_changed.h>
 
 LOG_MODULE_DECLARE(zmk, CONFIG_ZMK_LOG_LEVEL);
-
 
 #if DT_HAS_COMPAT_STATUS_OKAY(DT_DRV_COMPAT)
 
 static int board_status_layer_change_listener_cb(const zmk_event_t *eh) {
     ARG_UNUSED(eh);
     LOG_INF("Layer change event");
-    return 0;
+    return ZMK_EV_EVENT_BUBBLE;
 }
 
 ZMK_LISTENER(board_status_listener, board_status_layer_change_listener_cb);
